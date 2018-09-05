37676f2ed0def9c937a2f956438eba2895ad5e36
Angelo1211 <angelo12@vt.edu>
2018-09-05 14:41:53 +0200

    Added some git stuff and tested  MSVC compiler with Ninja builder.

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+// ===============================
+// AUTHOR       : Angel Ortiz (angelo12 AT vt DOT edu)
+// CREATE DATE  : 2018-07-02
+// PURPOSE      : Program initialization and shutdown
+// ===============================
+
+//#include "engine.h"
+#include "SDL.h"
+#include <stdio.h>
+
+int main( int argc, char* args[] ){
+    // Engine SSGE; //"Simple" Software Graphics Engine
+    // if(SSGE.startUp()){
+    //     SSGE.run();
+    // }
+    // else{
+    //     printf("SSGE could not initialize successfully. Shutting down.\n");
+    // }
+    // SSGE.shutDown();
+
+    printf("HRE could not load!\n");
+
+    return 0;
+}
